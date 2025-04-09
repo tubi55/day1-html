ecadf5d4946079aceaf298c2011127de0f94dd85
tubi55 <tubi55@nate.com>
2025-04-09 11:39:06 +0900

    feat: 모바일 패널 모션 연동

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 //메뉴 버튼 토글
 const btnToggle = document.querySelector(".btnToggle");
+const mobileMenu = document.querySelector(".mobileMenu");
 
 btnToggle.addEventListener("click", () => {
   btnToggle.classList.toggle("on");
+  mobileMenu.classList.toggle("on");
 });
