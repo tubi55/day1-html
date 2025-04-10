d4a40e862f99a537857a6c2a56dfca6205c7658d
tubi55 <tubi55@nate.com>
2025-04-10 11:45:20 +0900

    feat: 기본 폼항목 인증 처리

--- js/validation.js
+++ js/validation.js
@@ -1,15 +1,38 @@
-// js로 인증 처리할 폼요소 변수에 할당
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  // form안의 모든 정보 가져오기
   const formData = new FormData(form);
 
-  // 단일 요소는 get(name)
-  console.log(formData.get("gender"));
+  // 사용자 이름 인증시 메인이동, 그렇지 않으면 오류 콘솔 출력
+  if (formData.get("username").length < 2) {
+    e.preventDefault();
+    console.error("이름은 최소 2글자 이상 입력하세요.");
+  }
 
-  // 복수 요소는 getAll(name)
-  console.log(formData.getAll("myColor"));
+  //성별이 선택되어 있지 않으면 전송기능 막고 콘솔 에러 출력
+  if (!formData.get("gender")) {
+    e.preventDefault();
+    console.error("성별을 선택하세요.");
+  }
+
+  //학력이 선택되어 있지 않으면 이하 동일
+  if (!formData.get("edu")) {
+    e.preventDefault();
+    console.error("최종 학력을 선택하세요.");
+  }
+
+  //좋아하는 색상선택이 하나도 없으면 이하동일
+  if (formData.getAll("myColor").length === 0) {
+    e.preventDefault();
+    console.error("좋아하는 색상을 최소 하나 이상 선택하세요.");
+  }
+
+  //남기는 말은 최소글자 5글자 이상 최대글자수 10글자 이하일때 이하동일
+  if (
+    formData.get("comments").length < 4 ||
+    formData.get("comments").length >= 10
+  ) {
+    e.preventDefault();
+    console.error("나기는 말을 5글자 이상 10글자 미만으로 입력하세요.");
+  }
 });
