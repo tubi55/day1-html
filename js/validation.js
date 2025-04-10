69a1be3b060c8fa7273f4ad6b581070bfaf785f0
tubi55 <tubi55@nate.com>
2025-04-10 11:06:23 +0900

    feat: FormData클래스를 통한 효율적인 인풋값 추출

--- js/validation.js
+++ js/validation.js
@@ -1,25 +1,15 @@
 // js로 인증 처리할 폼요소 변수에 할당
 const form = document.querySelector("form");
-const username = form.querySelector("#username");
-const pwd1 = form.querySelector("#pwd1");
-const pwd2 = form.querySelector("#pwd2");
-const email = form.querySelector("#email");
-const edu = form.querySelector("#edu");
-const comments = form.querySelector("#comments");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const colors = form.querySelectorAll("input[type='checkbox']:checked");
+  // form안의 모든 정보 가져오기
+  const formData = new FormData(form);
 
-  if (colors.length === 0) {
-    console.error("체크된 요소가 없습니다.");
-    return;
-  }
+  // 단일 요소는 get(name)
+  console.log(formData.get("gender"));
 
-  console.log(colors); // ['red','green']
-
-  colors.forEach((data, index) => {
-    console.log(data.value);
-  });
+  // 복수 요소는 getAll(name)
+  console.log(formData.getAll("myColor"));
 });
