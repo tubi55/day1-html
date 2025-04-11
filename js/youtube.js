8a9ae21aeb5794faac00def4a1dcfb3ad63d2b9d
tubi55 <tubi55@nate.com>
2025-04-11 09:46:33 +0900

    유튜브 중간과정

--- js/youtube.js
+++ js/youtube.js
@@ -4,19 +4,49 @@ const base_url = "https://www.googleapis.com/youtube/v3/playlistItems";
 const num = 3;
 const req_url = `${base_url}?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
+const frame = document.querySelector(".frame");
+
 //요청 url을 인자로 전달해서 서버에 데이터 요청후 가져오기
 fetch(req_url)
   .then((data) => {
-    //모든 데이터가 받아지면 data라는 매개변수로 전달됨
-    //js로 가공가능한 json형태로 변환시킴
     return data.json();
   })
   .then((json) => {
-    //json데이터 변환이 완료되면 최종 데이터 콘솔로 확인
     const youtubeData = json.items;
+
+    let tags = "";
 
     youtubeData.forEach((data, index) => {
       console.log(data);
-      console.log(data.snippet.title);
+
+      let tit = "";
+      let desc = "";
+
+      if (data.snippet.title.length > 40) {
+        tit = data.snippet.title.substring(0, 40) + "...";
+      } else {
+        tit = data.snippet.title;
+      }
+
+      // (조건식)?참일때 실행구문 : 거짓일때 실행구문;
+      data.snippet.description.length > 120
+        ? (desc = data.snippet.description.substring(0, 120) + "...")
+        : (desc = data.snippet.description);
+
+      tags += `
+        <article>
+          <h2>${tit}</h2>
+          <figure>
+            <img src=${data.snippet.thumbnails.high.url} />
+          </figure>
+          <p>${desc}</p>
+          <span>${data.snippet.publishedAt
+            .split("T")[0]
+            .split("-")
+            .join(".")}</span>
+        </article>      
+      `;
     });
+
+    frame.innerHTML = tags;
   });
