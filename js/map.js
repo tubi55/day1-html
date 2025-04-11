026d78427e9d8671e3b705b46d308ebb3d68d6a4
tubi55 <tubi55@nate.com>
2025-04-11 14:46:41 +0900

    카카오 지도 완성

---
+++ js/map.js
@@ -0,0 +1,33 @@
+const mapContainer = document.querySelector("#map");
+const mapOption = {
+  center: new kakao.maps.LatLng(37.512723468898294, 127.06076770855582),
+};
+renderMap();
+window.addEventListener("resize", renderMap);
+
+function renderMap() {
+  mapContainer.innerHTML = "";
+  const map = new kakao.maps.Map(mapContainer, mapOption);
+  const marker = new kakao.maps.Marker({ position: mapOption.center });
+  marker.setMap(map);
+}
+
+const viewContainer = document.querySelector("#view");
+const view = new kakao.maps.Roadview(viewContainer);
+const viewMapper = new kakao.maps.RoadviewClient();
+
+viewMapper.getNearestPanoId(mapOption.center, 60, (panoId) => {
+  view.setPanoId(panoId, mapOption.center);
+});
+
+const [btnMap, btnView] = document.querySelectorAll(".btns li");
+
+btnMap.addEventListener("click", (e) => {
+  mapContainer.classList.add("on");
+  viewContainer.classList.remove("on");
+});
+
+btnView.addEventListener("click", (e) => {
+  mapContainer.classList.remove("on");
+  viewContainer.classList.add("on");
+});
