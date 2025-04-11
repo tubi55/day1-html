1bc7a6506274f1938bb187a15b8c66af15c42296
tubi55 <tubi55@nate.com>
2025-04-11 11:07:06 +0900

    유튜브 중간 단계

--- js/youtube.js
+++ js/youtube.js
@@ -3,7 +3,6 @@ const pid = "PLCnC6XcXem90DKJt20wOFhZLkYiXXZtG0";
 const base_url = "https://www.googleapis.com/youtube/v3/playlistItems";
 const num = 3;
 const req_url = `${base_url}?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
-
 const frame = document.querySelector(".frame");
 
 fetch(req_url)
@@ -21,8 +20,8 @@ fetch(req_url)
       let tit = "";
       let desc = "";
 
-      if (data.snippet.title.length > 40) {
-        tit = data.snippet.title.substring(0, 40) + "...";
+      if (data.snippet.title.length > 35) {
+        tit = data.snippet.title.substring(0, 35) + "...";
       } else {
         tit = data.snippet.title;
       }
@@ -34,7 +33,7 @@ fetch(req_url)
       tags += `
         <article>
           <h2>${tit}</h2>
-          <figure>
+          <figure class='pic' data-vid=${data.snippet.resourceId.videoId}>
             <img src=${data.snippet.thumbnails.high.url} />
           </figure>
           <p>${desc}</p>
@@ -47,4 +46,27 @@ fetch(req_url)
     });
 
     frame.innerHTML = tags;
+
+    const popup = document.querySelector(".popup");
+    const popupCon = popup.querySelector(".con");
+    const pics = document.querySelectorAll(".pic");
+    const closePop = document.querySelector(".closePop");
+
+    pics.forEach((data, index) => {
+      data.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        console.dir(data);
+        const vidId = data.getAttribute("data-vid");
+        console.log(vidId);
+
+        popup.style.display = "block";
+        popupCon.innerHTML = `<iframe src="http://www.youtube.com/embed/${vidId}"></iframe>`;
+      });
+    });
+
+    closePop.addEventListener("click", (e) => {
+      e.preventDefault();
+      popup.style.display = "none";
+    });
   });
