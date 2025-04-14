17cac93a16c12a59329a7634429ad41be7c0afe4
tubi55 <tubi55@nate.com>
2025-04-14 09:11:54 +0900

    feat: js를 통해 1rem의 기본 단위값 변경

--- js/textSizer.js
+++ js/textSizer.js
@@ -3,9 +3,13 @@ const [btnSmaller, btnBigger] = document.querySelectorAll(".btns button");
 let size = 16;
 
 btnSmaller.addEventListener("click", () => {
+  // 글자의 기본 1rem크기가 12px 미만이 되지 않도록 처리
+  if (size < 12) return;
   document.documentElement.style.fontSize = --size + "px";
 });
 
 btnBigger.addEventListener("click", () => {
+  // 글자의 기본 1rem크기가 20px을 초과하지 않도록 처리
+  if (size > 20) return;
   document.documentElement.style.fontSize = ++size + "px";
 });
